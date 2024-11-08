85087f8390e314e1cc67f50c5f35930bcda9176a
soni712 <62658911+soni712@users.noreply.github.com>
2024-11-08 11:18:52 +0000

    Variables & Datatypes

---
+++ 01Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = 'kanu@gmail.com';
+var accountPassword = '1234';
+let accountState ;
+accountCity ='Jaipur';
+// accountId = 2; Not allowed
+accountEmail = 'Kbhandari@gmail.com';
+accountPassword ='909090';
+console.table([accountId,accountEmail,accountPassword,accountState])
+//Preferred not to use vare because of block and functional scope
+
+
+
---
+++ 01Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+//ECMAScript - when you use newer version of js you use "use strict"
+"use strict" //Treat all js code as newer version
+// There are two types of Datatypes : -
+// 1. Primitive
+//number => 2 to the power 53
+//bigint
+//string =>''
+//boolean => true or false
+// null => Stand Alone value (Represenatation of emmpty value);u  intentionally telling it is empty;
+// undefined => value is not assigned yet
+//symbol => Use for uniqueness
+
+// 2. Non -primitive
+// Object
+
+//interview question we define tyoe of data by typeOf and typeof null is object and undefined is undefined.
+ 
