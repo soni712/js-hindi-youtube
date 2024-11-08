1fc6bf41eb38b31032c52b5f43e864009caeee4f
soni712 <62658911+soni712@users.noreply.github.com>
2024-11-08 11:46:32 +0000

    conversion of datatypes

---
+++ 01Basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+//Convert Into Number
+// let score = null;  -- 0
+// let score = undefined --NaN
+// let score = true --1
+// let score ='kanu' //--NaN
+
+// console.log(typeof score);
+// let convert = Number(score);
+// console.log(typeof convert, convert);
+
+// Convert Into Boolean
+// ''-- false
+// In Case of 0 false and in other no case true - 'kanu' = true
+// let isLoggenIn = 890
+// let booleanIsLoggedIn = Boolean(isLoggenIn);
+// console.log(booleanIsLoggedIn)
+
+//Convert into string
+// let someNumber = 99;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber)
+
+
